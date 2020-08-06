bcb847ede77511c1c43f05eefbe40824cf66bf90
Vitor Rafael <vitorafael.r.c@gmail.com>
2020-08-06 08:44:16 -0300

    finalizando-front-ent

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,34 +1,55 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import './styles.css'
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Horário</label>
-            <input type="text" id="time" />
-          </div>
+          <Select 
+            name="subject"
+            label="Matéria"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Química', label: 'Química'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'Inglês', label: 'Inglês'},
+              {value: 'Geografia', label: 'Geografia'},
+              {value: 'História', label: 'História'},
+              {value: 'Português', label: 'Português'},
+              {value: 'Literatura', label: 'Literatura'},
+              {value: 'Ed. Física', label: 'Ed. Física'},
+            ]}
+          />
+          <Select 
+            name="week_day" 
+            label="Dia da semana"
+            options={[
+              {value: '0', label: 'Domingo'},
+              {value: '1', label: 'Segunda'},
+              {value: '2', label: 'Terça'},
+              {value: '3', label: 'Quarta'},
+              {value: '4', label: 'Quinta'},
+              {value: '5', label: 'Sexta'},
+              {value: '6', label: 'Sabado'},
+            ]}
+          />
+          <Input type="time" label="Horário" name="time" />
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem></TeacherItem>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
       </main>
     </div>
   )
